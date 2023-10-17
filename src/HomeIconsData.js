49892eefa1d26ef4fa3a836c5126dda447fd96a2
sssssqew <sunrise.in.my.dream@gmail.com>
2023-10-17 16:41:16 +0900

    빌드 3

--- src/HomeIconsData.js
+++ src/HomeIconsData.js
@@ -3,32 +3,32 @@
 const homeIcons = [
     {
         iconTitle: '재밌게놀자', // '놀멍쉬멍'으로 변경?
-        iconSrc: '../assets/images/play.png', // 'images/play.png'에서 변경
+        iconSrc: '/assets/images/play.png', // 'images/play.png'에서 변경
         url: '/play'
     },
     {
         iconTitle: '일하자',
-        iconSrc: '../assets/images/work.png',
+        iconSrc: '/assets/images/work.png',
         url: '/work'
     },
     {
         iconTitle: '밥먹자', //'뭐먹지'로 변경? 
-        iconSrc: '../assets/images/food.png',
+        iconSrc: '/assets/images/food.png',
         url: '/food'
     },
     {
         iconTitle: '세상에이런일이',
-        iconSrc: '../assets/images/newspaper.png',
+        iconSrc: '/assets/images/newspaper.png',
         url: '/news'
     },
     {
         iconTitle: '집에가자', // '칼퇴하자'로 변경?
-        iconSrc: '../assets/images/newhome.png', // 'images/home.png'에서 변경
+        iconSrc: '/assets/images/newhome.png', // 'images/home.png'에서 변경
         url: '/backhome'
     },
     {
         iconTitle: '만남의광장',
-        iconSrc: '../assets/images/chat.png',
+        iconSrc: '/assets/images/chat.png',
         url: '/lounge'
     },
     {
